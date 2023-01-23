2b19cefb475b4ccd77ec844bced719338432d982
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2023-01-23 13:45:52 -0300

    tailwind

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,20 @@
-function Button({children}){
-    return <button>{children}</button>
+import PropTypes from 'prop-types';
+
+function Button({children, primary, secondary, success, warning, danger, outline, rounded}){
+    return <button className='px-3 py-1.5 border border-blue-600 bg-blue-500'>{children}</button>
 }
 
+Button.propTypes = {
+    checkVariationValue: ({primary, secondary, success, warning, danger, outline, rounded}) => {
+        const count = Number(!!primary)
+        + Number(!!secondary)
+        + Number(!!success)
+        + Number(!!warning)
+        + Number(!!danger)
+
+        if(count > 1){
+            return new Error('Only one of primary, secondary, success, warning, danger can be true');
+        }
+    }
+};
 export default Button;
