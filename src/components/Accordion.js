e91148dfbabe0b2b63ac6b8f0a489b7c7eb7dc53
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2023-02-01 16:44:48 -0300

    conditional rendering

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,12 +1,22 @@
-import { render } from "@testing-library/react";
+import { useState } from "react";
 
 function Accordion({items}){
 
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
     const renderedItems = items.map((item, index)=> {
+
+        const isExpanded = index === expandedIndex;
+
+        if(index === expandedIndex){
+            console.log('expanded');
+        }else{
+            console.log('collapsed');
+        }
         return (
             <div key={item.id}>
                 <div>{item.label}</div>
-                <div>{item.content}</div>
+                {isExpanded && <div>{item.content}</div>}
             </div>
         );
     });
