49e15fbd3315140a00d7a1df3e4d195c48f7cfc1
MatheusEli <matheuselisouzaferreira@hotmail.com>
2023-02-05 22:00:06 -0300

    styling dropdown component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 function App() {
-    const options = [
-        {label: 'Red', value: 'red'},
-        {label: 'Green', value: 'green'},
-        {label: 'Blue', value: 'blue'}
-    ]
-    return <Dropdown options={options}/>
+  // eslint-disable-next-line
+  const [selection, setSelection] = useState(null);
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
+
+  const handleSelect = (option) => {
+    console.log("The new selected option is: " + option.label);
+    setSelection(option);
+  };
+  return (
+    <Dropdown
+      options={options}
+      onChange={handleSelect}
+      value={selection}
+    />
+  );
 }
 
 export default App;
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,49 @@
-function Dropdown(){
-    return <div>Dropdown</div>
+import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+
+function Dropdown({ options, onChange, value }) {
+  // eslint-disable-next-line
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    onChange(option);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        key={option.value}
+        onClick={() => {
+          handleOptionClick(option);
+        }}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className='text-lg'/>
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Dropdown;
