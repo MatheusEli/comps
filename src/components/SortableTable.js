76f181c6d2306288b1282d769ddc2e13f97fc6f1
Matheus Eli <you@example.com>
2023-02-14 11:53:47 -0300

    sorting data

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,45 +1,89 @@
 import { useState } from 'react';
 import Table from "./Table";
+import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
 
 function SortableTable(props) {
 
-    const { config } = props;
-    const { data } = props;
+    const { config, data } = props;
 
-    const [updatedTable, setUpdatedTable] = useState(data);
+    const [sortOrder, setSortOrder] = useState(null);
+    const [sortBy, setSortBy] = useState(null);
 
-    const getSortValue = (vegetable) => {
-        return vegetable.name;
-    }
+    const handleClickOrder = (label) => {
 
-    const handleClickOrder = () => {
-
-        const order = 'desc';
-
-        const updatedData = updatedTable.sort((a, b) => {
-
-            const valueA = getSortValue(a);
-            const valueB = getSortValue(b);
-
-            const reverseOrder = order === 'asc' ? 1 : -1;
-
-            if(typeof valueA === 'string'){
-                return valueA.localeCompare(valueB) * reverseOrder;
-            }else{
-                return (valueA - valueB) * reverseOrder;
-            } 
-        });
-
-        setUpdatedTable(updatedData);
+        if (sortBy && label !== sortBy) {
+            setSortOrder('asc');
+            setSortBy(label);
+            return;
+        }
+        if (sortOrder === null) {
+            setSortOrder('asc');
+            setSortBy(label);
+        } else if (sortOrder === 'asc') {
+            setSortOrder('desc');
+            setSortBy(label);
+        } else if (sortOrder === 'desc') {
+            setSortOrder(null);
+            setSortBy(null);
+        }
     };
 
     const updatedConfig = config.map((column) => {
         if (!column.sortValue) {
             return column;
         }
-        return { ...column, header: () => { return <th className="bg-red-500" onClick={handleClickOrder}>{column.label}</th> } }
+        return {
+            ...column, header: () => {
+                return <th className="cursor-pointer hover:bg-gray-100" onClick={() => handleClickOrder(column.label)}>
+                    <div className='flex items-center'>
+                        {getIcons(column.label, sortBy, sortOrder)}
+                        {column.label}
+                    </div>
+                </th>
+            }
+        }
     });
-    return <Table {...props} data = {updatedTable} config={updatedConfig}/>;
+
+    let sortedData = data;
+
+    if (sortOrder && sortBy) {
+        const { sortValue } = config.find((column) => column.label === sortBy);
+
+        sortedData = [...data].sort((a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
+
+
+            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+            if (typeof valueA === 'string') {
+                return valueA.localeCompare(valueB) * reverseOrder;
+            } else {
+                return (valueA - valueB) * reverseOrder;
+            }
+        });
+    }
+    return <div><Table {...props} data={sortedData} config={updatedConfig} /></div>;
+}
+
+function getIcons(label, sortBy, sortOrder) {
+    if (label !== sortBy) {
+        return <div>
+            <GoArrowSmallUp />
+            <GoArrowSmallDown />
+        </div>;
+    }
+
+    if (sortOrder === null) {
+        return <div>
+            <GoArrowSmallUp />
+            <GoArrowSmallDown />
+        </div>;
+    } else if (sortOrder === 'asc') {
+        return <GoArrowSmallUp />;
+    } else if (sortOrder === 'desc') {
+        return <GoArrowSmallDown />;
+    }
 }
 
 export default SortableTable;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -7,20 +7,25 @@ function TablePage(){
         {name: 'Orange', color: 'bg-orange-500', score: 5},
         {name: 'Apple', color: 'bg-red-500', score: 3},
         {name: 'Banana', color: 'bg-yellow-500', score: 1},
-        {name: 'Lime', color: 'bg-green-500', score: 4}
+        {name: 'Lime', color: 'bg-green-500', score: 4},
+        {name: 'Cherry', color: 'bg-red-700', score: 2.5}
     ];
 
     const config = [
         {label: 'Fruits', sortValue: (fruit) => fruit.name, render: (fruit) => fruit.name},
         {label: 'Color', render: (fruit) => <div className={`p-2 m-3 ${fruit.color}`}/>},
         {label: 'Score', sortValue: (fruit) => fruit.score, render: (fruit) => fruit.score},
+        {label: 'Score Squared', sortValue: (fruit) => fruit.score ** 2, render: (fruit) => fruit.score ** 2},
     ];
 
     const keyFn = (fruit) => {
         return fruit.name;
     };
 
-    return <SortableTable data={data} config={config} keyFn={keyFn}/>
+    return <div>
+        <Table data={data} config={config} keyFn={keyFn}/>
+        <SortableTable data={data} config={config} keyFn={keyFn}/>
+    </div>
 }
 
 export default TablePage;
